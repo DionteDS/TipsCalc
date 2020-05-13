f5b681b42edab192623005a37ec04b79c7bedc3e
Dionte <Dionte_sb@yahoo.com>
2020-05-13 16:54:50 -0700

    Initial commit

---
+++ TipsCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipsCalc
+//
+//  Created by Dionte Silmon on 5/13/20.
+//  Copyright © 2020 Dionte Silmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
